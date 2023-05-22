b7115124b52c86e951c1c059d3c00519b54f4919
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-22 23:13:09 +0400

    Add technical task solution

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortCallback = (a: UserType, b: UserType) => a.name > b.name ? 1 : -1;
+            const reverseSortCallback = (a: UserType, b: UserType) => a.name < b.name ? 1 : -1;
+            switch (action.payload) {
+                case 'up':
+                    return [...state].sort(sortCallback);
+                case 'down':
+                    return [...state].sort(reverseSortCallback);
+                default: return state;
+            }
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(person => person.age >= action.payload); // need to fix
         }
         default:
             return state
