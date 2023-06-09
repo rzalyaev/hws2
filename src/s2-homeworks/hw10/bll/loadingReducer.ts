a628011781a4d51448b592112734ac83cde89514
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-09 17:17:27 +0400

    Add technical task solution

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,16 @@
+export type loadingReducerType = {
+    isLoading: boolean
+}
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ReturnType<typeof loadingAC>): loadingReducerType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading};
         default:
             return state
     }
