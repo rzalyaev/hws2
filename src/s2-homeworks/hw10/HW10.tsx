ff781804f95b90ae92b7aaa6ed85460890007f29
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-09 17:19:25 +0400

    Add container with its own styles

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -3,6 +3,7 @@ import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import s from './HW10.module.css';
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
@@ -30,18 +31,20 @@ const HW10 = () => {
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
+                <div className={s.container}>
+                    {isLoading ? (
+                        <div id={'hw10-loading'}>
+                            <Loader/>
+                        </div>
+                    ) : (
+                        <SuperButton
+                            id={'hw10-button-start-loading'}
+                            onClick={setLoading}
+                        >
+                            Set loading...
+                        </SuperButton>
+                    )}
+                </div>
             </div>
         </div>
     )
