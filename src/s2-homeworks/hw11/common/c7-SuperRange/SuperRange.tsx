20d456f38b0160ef2553a89102a85188abceb84c
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-13 16:13:59 +0400

    Add technical task solution

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+import {lime} from "@mui/material/colors";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
+    const green = lime[500]
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 147,
+                color: green
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
